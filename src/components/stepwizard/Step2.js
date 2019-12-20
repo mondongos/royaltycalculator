91a7684cf41f692efd90389c90cae6ecc0658a4e
Max Van Aken <mtvanaken@gmail.com>
2019-12-20 12:48:01 +0000

    Step 2

--- src/components/stepwizard/Step2.js
+++ src/components/stepwizard/Step2.js
@@ -8,29 +8,14 @@ export default class Step2StreamingInfo extends React.Component {
         return (
             <div>
                 <input
-                id="songName"
+                id="year1"
                 name="songName"
                 type="text"
                 placeholder="Song Title"
                 value={this.props.songName}
                 onChange={this.props.handleChange}
                 />
-                <input
-                id="artistName"
-                name="artistName"
-                type="text"
-                placeholder="Artist"
-                value={this.props.artistName}
-                onChange={this.props.handleChange}
-                />
-                <input
-                id="yearReleased"
-                name="yearReleased"
-                type="number"
-                placeholder="Year released"
-                value={this.props.yearReleased}
-                onChange={this.props.handleChange}
-                />
+                
             </div>
         )
     }
